1e0ab0ff111068466644f7888c2872caa00e57e2
esgn <5435148+esgn@users.noreply.github.com>
2022-11-15 12:13:41 +0100

    update, translation and new key

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # pva-ign
 
-Script basique de téléchargement des PVAs disponibles sur [remonterletemps.ign.fr](https://remonterletemps.ign.fr)
+Basic python script to download [IGNF](https://www.ign.fr/institut/identity-card) aerial shots from [remonterletemps.ign.fr](https://remonterletemps.ign.fr).
 
-La zone de recherche est à définir au début du script `main.py`.
-L'option `intersects_only`, également présente en début de fichier, permet de ne télécharger que les PVAs intersectant la zone de recherche. Avec la valeur `False`, cette option provoque le téléchargement des missions dans leur intégralité.
+## Configuration
+
+The area of interest for finding and downloading aerial shots must be defined in `properties.ini`. The area of interest is defined as a simple bounding box in longitude and latitude. In addition, the `only_intersecting_aerial_shots` option may also be set to `true` or `false` in `properties.ini`. When set to `true` only aerial shots interesecting the area of interest will be downloaded. When set to `false` the complete list of aerial shots for a given survey will be downloaded. 
+
+```ini
+[AREA OF INTEREST]
+minimum_longitude = 3.2558998675537105
+minimum_latitude = 47.36254089577267
+maximum_longitude = 3.301390132446289
+maximum_latitude = 47.37163871192848
+only_intersecting_aerial_shots = true
+```
 
 ## Usage
 
+```shell
+python3 -m venv venv/pva
+source venv/pva/bin/activate
+(pva) pip3 install -r requirements.txt
+(pva) python3 main.py
 ```
-pip3 install -r requirements.txt
-python3 main.py
-```
 
-Une interface en ligne de commande (utilisant [pick](https://github.com/wong2/pick)) permet de choisir la mission de son choix et de la télécharger. Sous Windows, télécharger en plus : `pip3 install windows-curses`
+A command line interface will allow to pick the aerial survey of interest based on its identifier or its year. 
 
-## TODO
+![pick survey](ui.png)
 
-- [ ] Gérer les erreurs éventuelles
-- [ ] Améliorer/Revoir l'interface
+Once selected the aerial shots will be downloaded in the `downloads` directory.
+
+![download jp2](ui2.png)
+
--- main.py
+++ main.py
@@ -4,117 +4,169 @@
 import sys
 import os
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import configparser
 import xml.etree.ElementTree as ET
 import json
-from pick import pick # nécessaire sous windows pour pick : pip install windows-curses
+from pick import pick
 from pathlib import Path
 import tqdm
 import time
-#from xml.dom import minidom
 from shapely.geometry import box, Polygon
 
-# Définition de la zone d'intérêt sous la forme d'une boundingbox
-min_lon=3.2558998675537105
-min_lat=47.36254089577267
-max_lon=3.301390132446289
-max_lat=47.37163871192848
-bbox = box(min_lon,min_lat,max_lon,max_lat)
+# Define retry behaviour for requests
+retry_strategy = Retry(
+    total=5,
+    status_forcelist=[429, 500, 502, 503, 504],
+    allowed_methods=["GET"],
+    backoff_factor=1
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = requests.Session()
+http.mount("https://", adapter)
+http.mount("http://", adapter)
 
-# Récupération uniquement des PVA intersectant la zone d'intérêt
-# Avec la valeur False, les missions sont récupérées dans leur intégralité
-intersects_only=True
+# URL stubs for kml and jp2 access
+kml_stub = "https://wxs.ign.fr/2ne3yvigrf4y78kmd5o2gp9v/dematkml/DEMAT.PVA/"
+jp2_stub = "https://wxs.ign.fr/2ne3yvigrf4y78kmd5o2gp9v/jp2/DEMAT.PVA/"
+referer = "ignfab.ign.fr"
 
-# Récupération de la configuration
+# Get parameters from configuration
 config = configparser.ConfigParser()
 config.read('properties.ini')
+min_lon = config.getfloat('AREA OF INTEREST', 'minimum_longitude')
+min_lat = config.getfloat('AREA OF INTEREST', 'minimum_latitude')
+max_lon = config.getfloat('AREA OF INTEREST', 'maximum_longitude')
+max_lat = config.getfloat('AREA OF INTEREST', 'maximum_latitude')
+bbox = box(min_lon, min_lat, max_lon, max_lat)
+intersects_only = config.getboolean(
+    'AREA OF INTEREST', 'only_intersecting_aerial_shots')
 
-def make_bbox_polygon(): # Pour inclusion dans les requêtes
-	b = [min_lat, min_lon, min_lat, max_lon, max_lat, max_lon, max_lat, min_lon, min_lat, min_lon]
-	b = [str(f) for f in b]
-	return ','.join([ '+'.join([ s for s in b[2*i:2*(i+1)] ]) for i in range(len(b)//2) ])
 
-def get_missions_count(p): # Récupération du nombre de missions sur la zone
-	r = requests.get(config['WFS']['CountUrl'].replace('#BBOX',p))
-	root = ET.fromstring(r.text)
-	return root.attrib['numberOfFeatures']
+def make_bbox_polygon():
+    """Bouding box to polygon coordinates"""
+    b = [min_lat, min_lon, min_lat, max_lon, max_lat,
+         max_lon, max_lat, min_lon, min_lat, min_lon]
+    b = [str(f) for f in b]
+    return ','.join(['+'.join([s for s in b[2*i:2*(i+1)]]) for i in range(len(b)//2)])
 
-def get_missions(p): # Récupération du détail des missions sur la zone
-	missions_details = []
-	r = requests.get(config['WFS']['MissionsUrl'].replace('#BBOX',p))
-	d = json.loads(r.text)
-	for row in d['features']:
-		if row['properties']['jp2']:
-			missions_details.append([ row['properties']['pv_date'], row['properties']['kml_layer_id']])
-	return missions_details
 
-def show_menu(m): # L'utilisateur sélectionne une mission dans la liste
-        title = 'Choisir une mission pour en récupérer les PVA : '
-        options = [i[1] +'(' + i[0].split('-')[0] + ')' for i in m]
-        option, index = pick(options, title)
-        return index
+def get_missions_count(p):
+    """Get aerial survey count"""
+    count_url = (
+        "https://wxs.ign.fr/search/layers?"
+        f"&cql_filter=demat_layer_id+=+'DEMAT.PVA$GEOPORTAIL:DEMAT;PHOTOS'+and+INTERSECTS(the_geom,POLYGON(({p})))"
+        "&outputFormat=json"
+        "&request=GetFeature"
+        "&resultType=hits"
+        "&typeName=ign:missions"
+        "&version=1.1.0"
+    )
+    r = http.get(count_url)
+    root = ET.fromstring(r.text)
+    return root.attrib['numberOfFeatures']
 
-def intersects(poly_coords): # True si le polygone correspondant a la PVA intersecte la zone d'intérêt
-	p = poly_coords.split(" ")
-	pt = [ tuple([float(j) for j in i.split(',')]) for i in p]
-	pg = Polygon(pt)
-	return bbox.intersects(pg)
 
-def kml_walk(kml_id): # Parcours de l'arbre des KMLs
-        leaves = [kml_id+'.kml']
-        jp2s_ids = []
-        while leaves:
-                l,j = get_leaves_and_jp2s(leaves)
-                jp2s_ids += j
-                leaves=l
-        return jp2s_ids
+def get_missions(p):
+    """Get survey metadata"""
+    missions_details = []
+    missions_url = (
+        "https://wxs.ign.fr/search/layers?"
+        f"&cql_filter=demat_layer_id+=+'DEMAT.PVA$GEOPORTAIL:DEMAT;PHOTOS'+and+INTERSECTS(the_geom,POLYGON(({p})))"
+        "&outputFormat=json"
+        "&propertyName=jp2,kml_layer_id,pv_date,title"
+        "&request=GetFeature"
+        "&sortBy=pv_date,kml_layer_id"
+        "&typeName=ign:missions"
+        "&version=1.1.0"
+    )
+    r = http.get(missions_url)
+    d = json.loads(r.text)
+    for row in d['features']:
+        if row['properties']['jp2']:
+            missions_details.append(
+                [row['properties']['pv_date'], row['properties']['kml_layer_id']])
+    return missions_details
 
-def get_leaves_and_jp2s(leaves_list): # Parcours d'un niveau de l'arbre
-	leaves, jp2s = ([] for i in range(2))
-	for l in leaves_list:
-		context = l.rpartition('/')[0]+'/' if ('/' in l) else ''
-		r = requests.get(config['KML']['UrlStub'] + l, headers={'referer': config['KML']['Referer']})
-		# Décommenter l'import de minidom et les lignes suivantes pour 
-		# télécharger les kmls intermédiaires pour mieux comprendre ou débuguer
-		#file = open(l.replace('/','_')+'.xml','w')
-		#xmlstr = minidom.parseString(r.text).toprettyxml(indent="   ")
-		#file.write(xmlstr)
-		#file.close()
-		root = ET.fromstring(r.text)
-		xleaves = root.findall(".//{http://www.opengis.net/kml/2.2}href")
-		leaves += [context + i.text for i in xleaves]
-		for p in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
-			poly = p.find('{http://www.opengis.net/kml/2.2}Polygon/{http://www.opengis.net/kml/2.2}outerBoundaryIs/{http://www.opengis.net/kml/2.2}LinearRing/{http://www.opengis.net/kml/2.2}coordinates')
-			if not intersects_only or intersects(poly.text):
-				xjp2 = p.find("{http://www.opengis.net/kml/2.2}ExtendedData/{http://www.opengis.net/kml/2.2}Data[{http://www.opengis.net/kml/2.2}displayName='JP2']/{http://www.opengis.net/kml/2.2}value")
-				jp2s.append(xjp2.text)
-	return leaves,jp2s
 
-def check_and_make_dirs(mission_id): # Création du répertoire de téléchargement
-	Path(os.path.join("downloads",mission_id)).mkdir(parents=True, exist_ok=True)
+def show_menu(m):
+    """Select survey"""
+    title = 'Choose an aerial survey to download its aerial shots:'
+    options = [i[1] + '(' + i[0].split('-')[0] + ')' for i in m]
+    option, index = pick(options, title, indicator="=>")
+    return index
 
-def download(jp2s,mission_id): # Téléchargement des clichés
-	check_and_make_dirs(mission_id)
-	for j in tqdm.tqdm(jp2s):
-		url = config['KML']['DlStub'] + mission_id + '/' + j + '.jp2'
-		r = requests.get(url, headers={'referer': config['KML']['Referer']})
-		with open('downloads/'+mission_id+'/'+j+'.jp2' , 'wb') as f:
-			f.write(r.content)
-		time.sleep(1) # soyons sympa avec le service de téléchargement des PVA 
+
+def intersects(poly_coords):
+    """True if aerial shot intersects bounding box"""
+    p = poly_coords.split(" ")
+    pt = [tuple([float(j) for j in i.split(',')]) for i in p]
+    pg = Polygon(pt)
+    return bbox.intersects(pg)
+
+
+def kml_walk(kml_id):
+    """Scan kml tree"""
+    leaves = [kml_id+'.kml']
+    jp2s_ids = []
+    while leaves:
+        l, j = get_leaves_and_jp2s(leaves)
+        jp2s_ids += j
+        leaves = l
+    return jp2s_ids
+
+
+def get_leaves_and_jp2s(leaves_list):
+    """Scan kml leaves"""
+    leaves, jp2s = ([] for i in range(2))
+    for l in leaves_list:
+        context = l.rpartition('/')[0]+'/' if ('/' in l) else ''
+        r = http.get(kml_stub + l, headers={'referer': referer})
+        root = ET.fromstring(r.text)
+        xleaves = root.findall(".//{http://www.opengis.net/kml/2.2}href")
+        leaves += [context + i.text for i in xleaves]
+        for p in root.iter('{http://www.opengis.net/kml/2.2}Placemark'):
+            poly = p.find(
+                '{http://www.opengis.net/kml/2.2}Polygon/{http://www.opengis.net/kml/2.2}outerBoundaryIs/{http://www.opengis.net/kml/2.2}LinearRing/{http://www.opengis.net/kml/2.2}coordinates')
+            if not intersects_only or intersects(poly.text):
+                xjp2 = p.find(
+                    "{http://www.opengis.net/kml/2.2}ExtendedData/{http://www.opengis.net/kml/2.2}Data[{http://www.opengis.net/kml/2.2}displayName='JP2']/{http://www.opengis.net/kml/2.2}value")
+                jp2s.append(xjp2.text)
+    return leaves, jp2s
+
+
+def check_and_make_dirs(mission_id):
+    """Create download directory"""
+    Path(os.path.join("downloads", mission_id)).mkdir(
+        parents=True, exist_ok=True)
+
+
+def download(jp2s, mission_id):
+    """Download aerial shots for a given survey"""
+    check_and_make_dirs(mission_id)
+    for j in tqdm.tqdm(jp2s):
+        url = jp2_stub + mission_id + '/' + j + '.jp2'
+        with http.get(url, headers={'referer': referer}, stream=True) as r:
+            with open('downloads/'+mission_id+'/'+j+'.jp2', 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        time.sleep(1)  # let's be gentle with the download service
+
 
 def main():
-	p = make_bbox_polygon()
-	c = get_missions_count(p)
-	#print(str(c) + " missions trouvées sur la zone d'intérêt.")
-	if(int(c)>100):
-		print("Plus de 100 missions trouvées. Merci de restreindre la taille de la zone d'intérêt.")
-		sys.exit(0)
-	m = get_missions(p)
-	i = show_menu(m)
-	print("Chargement de la liste des clichés à télécharger")
-	jp2s = kml_walk(m[i][1])
-	print(str(len(jp2s)) + " clichés à charger pour la mission " + str(m[i][1]))
-	download(jp2s,m[i][1])
+    p = make_bbox_polygon()
+    c = get_missions_count(p)
+    if (int(c) > 100):
+        print("More than 100 survey found. Please reduce bounding box.")
+        sys.exit(0)
+    m = get_missions(p)
+    i = show_menu(m)
+    print("Parsing KML files to get aerial shots download URLs. This could take some time.")
+    jp2s = kml_walk(m[i][1])
+    print(str(len(jp2s)) +
+          " aerial shots will be downloaded for IGNF survey " + str(m[i][1]))
+    download(jp2s, m[i][1])
 
 if __name__ == "__main__":
     main()
